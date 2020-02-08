0181bcd3a6367900a83c8afc429c7878e165f2a5
sherriefaylin <linsherrie@gmail.com>
2020-02-08 13:48:41 -0600

    sat feb 8 start

--- js/sketch.js
+++ js/sketch.js
@@ -3,8 +3,7 @@ var canvas;
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0,0);
-    canvas.style('z-index', '-1')
- 
+    canvas.style('z-index', '-1') 
     strokeWeight(8);
   }
 
@@ -15,14 +14,5 @@ function setup() {
         strokeWeight(8);
         stroke('black'); 
     line(pmouseX-5, pmouseY-5, mouseX-5, mouseY-5);
-
-  //  strokeWeight(0.5);
-  //   line(732, 876, 732, 0);
- 
-  //   stroke('gray'); 
-  //      strokeWeight(0.5);
-  //   line(0,438,1464,438);
-
-  //   stroke('gray');
   } 
 
